c7289272b4c6e369969d55d082e61654ffe418c9
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-07-21 04:25:33 -0700

    removed some bad words

--- toScratchList.py
+++ toScratchList.py
@@ -1,12 +1,11 @@
-fileName="Collins Scrabble Words (2019).txt"
+fileName="cleanWords.txt"
 output=''
 file=open(fileName,"r")
 #file.readline()
 #file.readline()
 for i in file:
-    if len(i)>=5: output+=i.strip()+"."
+    if len(i)>=3: output+=i.strip()+"."
 file.close()
-
 out=open("scratchOutput.txt","w")
 out.write(output)
 out.close()
